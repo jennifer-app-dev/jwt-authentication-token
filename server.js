222049a4c28793caab5c95f69a7d7f0f18bb1ac1
jennifer-app-dev <jennifer.app.dev@gmail.com>
2021-06-15 11:21:26 -0700

    Create sample GET request to test the url and install & configure the request.rest

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
+// 1. setup express server
 const express = require('express')
 const app = express()
 
+// 4. create posts
+const posts = [
+  {
+    username: "Jane",
+    title: "Post 1"
+  },
+  {
+    username: "John",
+    title: "Post 2"
+  }
+]
+
+// 3. create a simple route, get all the posts inside the app
+app.get('/posts', (req, res) => {
+  // 3.A: make it return => the post
+  res.json(posts)
+})
+
+// 2. app listen to port: 3000
 app.listen(3000)
