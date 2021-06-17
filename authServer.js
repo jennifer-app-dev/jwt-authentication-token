018c1f7207f47d8d841a3df01f5402ffc0932def
jennifer-app-dev <jennifer.app.dev@gmail.com>
2021-06-16 22:54:25 -0700

    Create the function store the refreshTokens

--- authServer.js
+++ authServer.js
@@ -11,10 +11,27 @@ const jwt = require('jsonwebtoken');
 // let app use the json from the body get passed in
 app.use(express.json())
 
+// PART. 4: Store the refreshTokens
+let refreshTokens = [];
+
 // PART.III: we need to create a new function
 app.post('/token', (req, res) => {
   // 1st, we wanna take in a refreshToken
   const refreshToken = req.body.token
+  // PART 4.2: check if the refreshTokens exist 
+  if(refreshToken == null) return res.sendStatus(401)
+  // PART 4.3: check the current refreshTokens includes the refreshTokens that sent to us.
+  if(!refreshTokens.includes(refreshTokens)) return res.sendStatus(403)
+
+  // PART 4.4: next, verify the refreshToken
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    // first, check the err first
+    if(err) return res.sendStatus(403)
+    // so, if statusCode is '403' then send accessToken
+    const accessToken = generateAccessToken({ name: user.name })
+    // 2nd, return the info
+    res.json({ accessToken: accessToken }) 
+  })
 })
 
 // // 4. create posts
@@ -53,6 +70,9 @@ app.post('/login', (req, res) => {
   
   // Next, create a fresh token (since the generateAccessToken will expire in 15 secs )
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+
+  // PART. 4: Store the token
+  refreshToken.push(refreshToken)
 
   // Lastly, return the refreshToken to our user.
   res.json({accessToken: accessToken, refreshToken: refreshToken })
